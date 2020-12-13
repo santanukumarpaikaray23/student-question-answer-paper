365d83c4ff70383c513d277cb18e4559b4d62545
SANTANU KUMAR PAIKARAY <paikaraysantanu1996@gmail.com>
2020-12-14 02:38:07 +0530

    add all file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectoneusingtwowaydatabinding';
+  messageone="Enter your answer"
+  getNameOne(){
+    console.log('1st Question Answer is');
+    console.log(this.messageone)
+  }
+  messagetwo="Enter your answer"
+  getNameTwo(){
+    console.log('2nd Question Answer is');
+    console.log(this.messagetwo)
+  }
+  messagethree="Enter your answer"
+  getNameThree(){
+    console.log('3rd Question Answer is');
+    console.log(this.messagethree)
+  }
+  messagefour="Enter your answer"
+  getNameFour(){
+    console.log('4th Question Answer is');
+    console.log(this.messagefour)
+  }
+  messagefive="Enter your answer"
+  getNameFive(){
+    console.log('5th Question Answer is');
+    console.log(this.messagefive)
+  }
+  messagesix="Enter your answer"
+  getNameSix(){
+    console.log('6th Question Answer is');
+    console.log(this.messagefive)
+  }
+
+
 }
